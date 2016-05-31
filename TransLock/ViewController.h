7d0d8705ac8ce1c3662a49fd4d60c001d24e05be
mohabg <mg250@duke.edu>
2016-05-30 22:29:11 -0400

    Initial Commit

---
+++ TransLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransLock
+//
+//  Created by Mohab Gabal on 5/30/16.
+//  Copyright © 2016 Mohab Gabal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
